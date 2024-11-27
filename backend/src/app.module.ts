c3383db2e1b3e75fb13f8f46358addd986f99d38
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-27 07:17:55 -0300

    feat: add prisma config and schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { BebidaModule } from './bebida/bebida.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule, BebidaModule],
   controllers: [AppController],
   providers: [AppService],
 })
